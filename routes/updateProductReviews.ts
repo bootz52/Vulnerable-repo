ba408ccb035f38580ba5743566347f90468577e7
bootz52 <marcbenassi@gmail.com>
2025-04-16 21:49:30 -0500

    Initialize vulns/sql-injection

---
+++ routes/updateProductReviews.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import challengeUtils = require('../lib/challengeUtils')
+import { type Request, type Response, type NextFunction } from 'express'
+import * as db from '../data/mongodb'
+import { challenges } from '../data/datacache'
+
+const security = require('../lib/insecurity')
+
+// vuln-code-snippet start noSqlReviewsChallenge forgedReviewChallenge
+module.exports = function productReviews () {
+  return (req: Request, res: Response, next: NextFunction) => {
+    res.json({})
+  }
+}
+// vuln-code-snippet end noSqlReviewsChallenge forgedReviewChallenge
